0c20ef6b096ebabeb36aa666ca0a763d93f90127
Oscar Sanchez <oscar@loungelogic.tv>
2020-05-12 12:27:03 +0300

    test no duplicate increments asdas

--- packages/web/src/App.js
+++ packages/web/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          release v1.0.0
+          release v2.0.0
         </p>
         <a
           className="App-link"
